8e93f4f56181f2759d5cd550f2a5d736db8ff283
Genuifx <1119127128@qq.com>
2018-11-05 16:12:45 +0800

    feat(redux): redux template for wxa

--- echart/wxa.config.js
+++ echart/wxa.config.js
@@ -1,10 +1,11 @@
 const UglifyjsPlugin = require('@wxa/plugin-uglifyjs');
 const ReplacePlugin = require('@wxa/plugin-replace');
+const CopyPlugin = require('@wxa/plugin-copy');
 let prod = process.env.NODE_ENV === 'production';
 
 // 环境变量
 const envlist = [{
-    'WXA_ENV': process.env.NODE_ENV || 'development'
+    'WXA_ENV': process.env.NODE_ENV || 'development',
 }];
 
 
@@ -12,6 +13,10 @@ module.exports = {
     plugins: [
         new ReplacePlugin({
             list: envlist,
+        }),
+        new CopyPlugin({
+            from: './src/echarts/img',
+            to: 'echarts/img',
         }),
     ],
 };
